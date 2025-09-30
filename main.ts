5ea933a5cf32b743db10c796ad7437ea6ab35429
default <bthistle@huronperthcatholic.ca>
2025-09-30 14:40:07 +0000

    added a string literal in TypeScript

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-09-30
+ * @fileoverview this program uses string literals.
+ */
+
+// string literal text
+console.log("I am learning computer science.");
+
+// string literal text
+console.log("I am enjoying computer science.");
+
+// string literal text
+console.log("I'm learning string literals.");
+
+console.log("\nDone");
