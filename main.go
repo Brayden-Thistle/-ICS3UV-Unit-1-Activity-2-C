7953fc2a1fcb17a9a08bfde52d5666a622856be9
default <bthistle@huronperthcatholic.ca>
2025-09-30 14:51:53 +0000

    added a literal string into Go.

---
+++ main.go
@@ -0,0 +1,23 @@
+/*
+* @Author Brayden Thistlr
+* @version 1.0.0
+* @date 2025-09-30
+* @fileoverview this program practices literal strings
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+//string literal text
+fmt.Println("I like computer science.")
+
+//string literal text
+fmt.Println("I love computer science.")
+
+//string literal text
+fmt.Println("I am learning computer science.")
+
+fmt.Println("\nDone")
+}
